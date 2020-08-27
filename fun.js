48bf748ee6252fd607e1f21eccfccf4b5a1619ec
amaracvg <amara.cvg@gmail.com>
2020-08-27 18:03:52 +0530

    functional program

--- fun.js
+++ fun.js
@@ -10,3 +10,33 @@ const getTea = (numOfCups)=>{
 }
 
 getTea(40);
+
+
+// Function that returns a string representing a cup of green tea
+const preparegreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const gettingTea = (prepareingTea, numCups) => {
+  const teaCup = [];
+
+  for(let ncups = 0; ncups < numCups; ncups++) {
+    const diffTeaCups = prepareingTea();
+    teaCup.push(diffTeaCups);
+  }
+
+  return (teaCup,);
+};
+
+// Only change code below this line
+const tea4GreenTeamFCC = gettingTea(preparegreenTea,27);
+console.log(tea4GreenTeamFCC)
+const tea4BlackTeamFCC = gettingTea(prepareBlackTea,13);
+console.log(tea4BlackTeamFCC)
+// Only change code above this line
